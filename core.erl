3a68480e02bdccc5d181dfa215b32a75212cc950
Gleicon Moraes <gleicon@parvati.(none)>
2010-01-05 15:32:28 -0200

    refactoring for proper Client use, some tidying and unpythonization

--- core.erl
+++ core.erl
@@ -1,20 +1,47 @@
 %% core restmq operations
+%% gleicon@gmail.com
+%% http://zenmachine.wordpress.com
+%% http://github.com/gleicon/restmq-erl
+%%
+%%
+%% Client = core:redis_connect("localhost", 6379).
+%% core:queue_add(Client, "tusca", "some value").
+%% core:queue_add(Client, "tusca", "another value").
+%% core:queue_len(Client, "tusca").
+%% core:queue_last_items(Client, "tusca").
+%% core:queue_get(Client, "tusca").
+%% core:queue_getdel(Client, "tusca").
+%% core:queue_len(Client, "tusca").
+%% core:queue_add(Client, "tusca", "some value").
+%% core:queue_add(Client, "tusca", "another value").
+%% core:queue_tail(Client, "tusca").
+%% core:queue_len(Client, "tusca").
+%% core:redis_close(Client).
+%%
+
 
 -module(core).
 -include("/opt/lib/erlang/lib/erldis-0.0.11/include/erldis.hrl").
 -compile(export_all).
 
--define (POLICY_BROADCAST,  "1").
--define (POLICY_ROUNDROBIN, "2").
+-define (POLICY_BROADCAST,  1).
+-define (POLICY_ROUNDROBIN, 2).
+
+redis_connect(Host, Port) ->
+    {ok, Client} = erldis:connect(Host, Port),
+    Client.
+
+redis_close(Client) ->
+    erldis_client:stop(Client).
 
 
+%% TODO: utf-8 normalization
 normalize(Key) ->
     Key.
 
-%% this is hackey, the most IFfy use of case/of
 queue_set_add(Client, Queue) ->
     Queue_name = Queue ++ ":queue",
-    erldis_sets:add_element(list_to_bitstring(Queue_name), Client, <<"ALLQUEUES">>).
+    {"ALLQUEUES", erldis_sets:add_element(list_to_bitstring(Queue_name), Client, <<"ALLQUEUES">>)}.
 
 queue_unique_id(Client, Queue) ->
     Key = Queue ++ ":UUID",
@@ -25,8 +52,7 @@ queue_unique_id(Client, Queue) ->
             {Queue, Id}
     end.
 
-queue_add(Queue, Data) ->
-    {ok, Client} = erldis:connect("localhost", 6379),
+queue_add(Client, Queue, Data) ->
     {Queue, Id} = queue_unique_id(Client, Queue),
     _ = queue_set_add(Client, Queue),
     Obj_key = lists:append([Queue, [$:], integer_to_list(Id)]),
@@ -34,17 +60,10 @@ queue_add(Queue, Data) ->
     %%Lp = erldis:lpush(Client, list_to_bitstring(Queue_name), list_to_bitstring(Obj_key)),
     Lp = erldis_list:in(list_to_bitstring(Obj_key), list_to_bitstring(Queue_name), Client),
     Sret = erldis:set(Client, Obj_key, list_to_bitstring(Data)),
-    erldis_client:stop(Client),
     {Queue, Obj_key, Lp, Sret}.
 
-queue_get(Queue) ->
-    queue_get(Queue, false).
-
-queue_get(Queue, GetDel) ->
-    {ok, Client} = erldis:connect("localhost", 6379),
-    Ret=queue_get(Client, Queue, GetDel),
-    erldis_client:stop(Client),
-    Ret.
+queue_get(Client, Queue) ->
+    queue_get(Client, Queue, false).
 
 queue_get(Client, Queue, GetDel) ->
     Queue_name = Queue ++ ":queue",
@@ -54,90 +73,81 @@ queue_get(Client, Queue, GetDel) ->
             {nil, GetDel, nil};
         Nextkey ->
             case GetDel of
-                    true ->
-                        OldKey = list_to_bitstring(Nextkey),
-                        NewKeyStr = Nextkey++":lock",
-                        NewKey = list_to_bitstring(NewKeyStr),
-                        RRet = erldis:rename(Client, OldKey, NewKey),
-                        Val = erldis:get(Client, NewKey),
-                        Ret = queue_del(Client, Queue, NewKeyStr),
-                        {Val, GetDel, Nextkey, Ret, RRet};
-                    _ ->
-                        Val = erldis:get(Client, list_to_bitstring(Nextkey)),
-                        {Val, GetDel, Nextkey}
-                end
+                true ->
+                    OldKey = list_to_bitstring(Nextkey),
+                    NewKeyStr = Nextkey++":lock",
+                    NewKey = list_to_bitstring(NewKeyStr),
+                    RRet = erldis:rename(Client, OldKey, NewKey),
+                    Val = erldis:get(Client, NewKey),
+                    Ret = queue_del(Client, Queue, NewKeyStr),
+                    {Val, GetDel, Nextkey, Ret, RRet};
+                _ ->
+                    Val = erldis:get(Client, list_to_bitstring(Nextkey)),
+                    {Val, GetDel, Nextkey}
+            end
     end.
 
 queue_del(Client, Queue, Key) ->
-    case erldis:del(Client, list_to_bitstring(Key)) of
-        [nil] ->
-            {Queue, Key, nil};
-        Ret ->
-            {Queue, Key, Ret}
-    end.
+    {Queue, Key, erldis:del(Client, list_to_bitstring(Key))}.
 
-queue_len(Queue) ->
-    {ok, Client} = erldis:connect("localhost", 6379),
+queue_len(Client, Queue) ->
     Queue_name = Queue ++ ":queue",
-    Ret = erldis_list:len(list_to_bitstring(Queue_name), Client),
-    erldis_client:stop(Client),
-    {Queue, Ret, Queue_name}.
+    {Queue, erldis_list:len(list_to_bitstring(Queue_name), Client), Queue_name}.
 
-queue_all() ->
-    %%smembers
-    {ok, Client} = erldis:connect("localhost", 6379),
-    QList = erldis_sets:to_list(Client, <<"ALLQUEUES">>),
-    erldis_client:stop(Client),
-    QList.
+queue_all(Client) ->
+    erldis_sets:to_list(Client, <<"ALLQUEUES">>).
 
-queue_getdel(Queue) ->
-    queue_get(Queue, true).
+queue_getdel(Client, Queue) ->
+    queue_get(Client, Queue, true).
 
-queue_policy_set(Queue, Policy) -> %%when Policy == ?POLICY_BROADCAST or Policy == ?POLICY_ROUNDROBIN ->
-    {ok, Client} = erldis:connect("localhost", 6379),
+%% TODO: guard against values outside POLICY_[BROADCAST, ROUNDROBIN]
+queue_policy_set(Client, Queue, Policy) -> %%when Policy == ?POLICY_BROADCAST or Policy == ?POLICY_ROUNDROBIN ->
     Queue_policy = Queue ++ ":queuepolicy",
-    Ret = erldis:set(Client, list_to_bitstring(Queue_policy), list_to_bitstring(Policy)),
-    erldis_client:stop(Client),
-    {Queue, Policy, Ret}.
+    {Queue, Policy, erldis:set(Client, list_to_bitstring(Queue_policy), list_to_bitstring([Policy]))}.
 
-queue_policy_get(Queue) ->
-    {ok, Client} = erldis:connect("localhost", 6379),
+queue_policy_get(Client, Queue) ->
     Queue_policy = Queue ++ ":queuepolicy",
-    Ret = erldis:get(Client, list_to_bitstring(Queue_policy)),
-    erldis_client:stop(Client),
-    {Queue, Ret}.
+    {Queue, erldis:get(Client, list_to_bitstring(Queue_policy))}.
 
+%%
 %% tail family
+%%
+queue_tail(Client, Queue) ->
+    queue_tail(Client, Queue, false, 10).
 
-queue_tail(Queue) ->
-    queue_tail(Queue, false, 10).
+queue_tail(Client, Queue, Delete) ->
+    queue_tail(Client, Queue, Delete, 10).
 
-queue_tail(Queue, Delete) ->
-    queue_tail(Queue, Delete, 10).
-
-queue_tail(Queue, Delete, KeyNo) ->
-    {ok, Client} = erldis:connect("localhost", 6379),
-    Ret = queue_tail_acc(Client, Queue, Delete, [], KeyNo),
-    erldis_client:stop(Client),
-    {Queue, Ret}.
+queue_tail(Client, Queue, Delete, KeyNo) ->
+    {Queue, queue_tail_acc(Client, Queue, Delete, [], KeyNo)}.
 
 queue_tail_acc(Client, Queue, Delete, List, N) when N > 0 ->
     {_, _, L} = queue_get(Client, Queue, Delete),
-    L2 = List ++ L,
-    queue_tail_acc(Client, Queue, Delete, L2, N-1);
+    case L of 
+        nil -> 
+            List;
+        L ->
+            L2 = List ++ L,
+            queue_tail_acc(Client, Queue, Delete, L2, N-1)
+    end;
+
 
 queue_tail_acc(_, _, _, List, 0) ->
     List.
 
-%% tail family
-
-queue_count_elements(Queue) ->
+%%
+%% count_elements uses KEY command... turns out to be expensive and cause of a lot of broken pipes. It's out for now
+%%
+queue_count_elements(Client, Queue) ->
     {Queue, 10}.
 
-queue_last_items(Queue) ->
-    queue_last_items(Queue, 10).
+%%
+%% last N items on the queue (non-destructive, but for stats purposes only)
+%%
+queue_last_items(Client, Queue) ->
+    queue_last_items(Client, Queue, 10).
     
-queue_last_items(Queue, Items) ->
-    {Queue, Items}.
-
+queue_last_items(Client, Queue, Items) ->
+    Queue_name = Queue ++ ":queue",
+    {Queue, erldis:lrange(Client, list_to_bitstring(Queue_name), 0, Items -1)}.
 
